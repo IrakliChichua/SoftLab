a84b035bb0c1268bc9c7123e1ce9c2bbc00a975f
Shacore <iraklichichua7@gmail.com>
2022-01-21 00:03:07 +0400

    .

--- Timer/src/Timer.js
+++ Timer/src/Timer.js
@@ -7,12 +7,13 @@ export default function Timer() {
     const [minutes, setMinutes] = useState(0)
     const [seconds, setSeconds] = useState(0)
     const [milSeconds, setMilSeconds] = useState(0)
+
     const [start_stop, setStart_Stop] = useState(false)
     const [showMilSecs, setShowMilSecs] = useState(false)
 
     const [totalTimeInMilSec, setTotalTimeInMilSec] = useState(3 * 100);
 
-    function secondsToTime() {
+    function milSecondsToTime() {
         setHours(Math.floor(totalTimeInMilSec / (60 * 60 * 100)));
 
         let divisor_for_minutes = totalTimeInMilSec % (60 * 60 * 100);
@@ -25,6 +26,17 @@ export default function Timer() {
         setMilSeconds(Math.ceil(divisor_for_mil_seconds))
     }
 
+    function format(milSec){
+        const seconds = Math.floor((milSec / 100) % 60);
+        const minutes = Math.floor((milSec / 100 / 60) % 60);
+        const hours = Math.floor((milSec  / 100 / 3600 ) % 24)
+        const milSeconds = Math.floor((milSec % 100))
+        return (hours<10 ? '0' + hours : hours) + ' : ' +
+               (minutes<10 ? '0' + minutes : minutes) + ' : ' +
+               (seconds<10 ? '0' + seconds : seconds) +
+               (showMilSecs ? (milSeconds<10 ? ' : 0' + milSeconds : ' : ' + milSeconds) : '');
+    }
+
     const onClickHandleStart = () => {
         setStart_Stop((prev) => !prev)
     }
@@ -34,7 +46,6 @@ export default function Timer() {
     const onClickHandleMilSeconds = () => {
         setShowMilSecs((prev) => !prev)
     }
-
 
     useEffect(() => {
         if (start_stop) {
@@ -46,19 +57,23 @@ export default function Timer() {
     }, [start_stop])
 
     useEffect(() => {
-        if (totalTimeInMilSec >= 0) {
-            secondsToTime()
-        }
+        if (totalTimeInMilSec >= 0)  milSecondsToTime()
         else return () => setStart_Stop((prev) => !prev)
+    }, [totalTimeInMilSec])
 
+    useEffect(() => {
+        if (totalTimeInMilSec === 1) return ()=> setStart_Stop((prev) => !prev)
     }, [totalTimeInMilSec])
 
     return (
         <div className="bg-dark text-white text-center pb-5">
-            <h2 className={"pt-5 pb-3 "}>
+            <h2 className={"pt-5 pb-3"}>
                 <span>{hours}</span> : <span>{minutes}</span> : <span>{seconds}</span>{showMilSecs ?
                 <span>{' : ' + milSeconds}</span> : null}
             </h2>
+            <h3 className={"pb-3"}>
+                {format(totalTimeInMilSec)}
+            </h3>
             <ButtonGroup>
                 {
                     totalTimeInMilSec <= 0
